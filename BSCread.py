58ddcf9b79092179ddcf0b76bbd5c18175ec5fc2
JayakrishnanGnair <jayakrishnan.g@live.com>
2022-03-20 01:44:01 -0500

    First version of BSC token read

--- BSCread.py
+++ BSCread.py
@@ -6,3 +6,22 @@ Created on Sat Mar 19 18:48:15 2022
 @author: jayakrishnangopalakrishnannair
 """
 
+from web3 import Web3
+import time, json
+bsc = 'https://bsc-dataseed.binance.org/'
+web3 = Web3(Web3.HTTPProvider(bsc))
+
+print(web3.isConnected())
+
+address = input("Enter your wallet address")
+
+
+if((web3.isAddress(address)) == "False"):
+    input("The address is not valid, enter a valid addres ")
+
+balance = web3.eth.getBalance(address)
+print(balance)
+
+result = web3.fromWei(balance, 'ether')
+print(result)
+ 
